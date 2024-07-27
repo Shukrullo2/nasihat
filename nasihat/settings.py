f265a2220a0a05698b9dedf55f22bfccf045619f
Shukrullo <94306881+Shukrullo2@users.noreply.github.com>
2024-07-27 15:53:53 +0500

    cors update

--- nasihat/settings.py
+++ nasihat/settings.py
@@ -61,8 +61,8 @@ MIDDLEWARE = [
     "django.middleware.clickjacking.XFrameOptionsMiddleware",
 ]
 
-# CORS_ALLOWED_ORIGINS = ["https://nasihat-ab617c06e45b.herokuapp.com", "https://50nasihat.vercel.app" , "http://127.0.0.1:3001"]
-CORS_ALLOW_ALL_ORIGINS = True
+CORS_ALLOWED_ORIGINS = ["https://nasihat-ab617c06e45b.herokuapp.com", "https://50nasihat.vercel.app" , "http://192.168.43.218:3000/"]
+# CORS_ALLOW_ALL_ORIGINS = True
 # CORS_ALLOW_CREDENTIALS = True
 
 CORS_ALLOW_METHODS = [
