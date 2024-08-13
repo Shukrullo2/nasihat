1c957930afcfa43d3c6cd8851d12165cedac81e5
Shukrullo <94306881+Shukrullo2@users.noreply.github.com>
2024-08-13 22:29:38 +0500

    Deployment on Azure

---
+++ nasihat/deployment.py
@@ -0,0 +1,35 @@
+import os
+from .settings import *
+from .settings import BASE_DIR as base_dir
+
+ALLOWED_HOSTS = [os.environ['WEBSITE_HOSTNAME']]
+CSRF_TRUSTED_ORIGINS = ['https://'+os.environ['WEBSITE_HOSTNAME']]
+DEBUG = False
+
+MIDDLEWARE = [
+    "django.middleware.security.SecurityMiddleware",
+    "django.contrib.sessions.middleware.SessionMiddleware",
+    "whitenoise.middleware.WhiteNoiseMiddleware",
+    "corsheaders.middleware.CorsMiddleware",
+    "django.middleware.common.CommonMiddleware",
+    "django.middleware.csrf.CsrfViewMiddleware",
+    "django.contrib.auth.middleware.AuthenticationMiddleware",
+    "django.contrib.messages.middleware.MessageMiddleware",
+    "django.middleware.clickjacking.XFrameOptionsMiddleware",
+]
+
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+STATIC_ROOT = os.path.join(base_dir, 'staticfiles')
+
+connection_string = os.environ['AZURE_POSTGRESQL_CONNECTIONSTRING']
+parameters = {pair.split('='): pair.split('=')[1] for pair in connection_string.split(' ')}
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backend.postgresql',
+        'NAME': parameters['dbname'],
+        'HOST': parameters['host'],
+        'USER': parameters['user'],
+        'PASSWORD': parameters['password'],
+    }
+}
