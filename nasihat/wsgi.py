a400f9a984a8f9ee88e66f24ab5d9bcf6794b536
Shukrullo <94306881+Shukrullo2@users.noreply.github.com>
2024-10-08 17:55:09 +0500

    fix

--- nasihat/wsgi.py
+++ nasihat/wsgi.py
@@ -11,7 +11,7 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-settings_module = "nasihat.deployment" if 'WEBSITE_HOSTNAME' in os.environ else "nasihat.settings"
+settings_module = "nasihat.deployment" if "WEBSITE_HOSTNAME" in os.environ else "nasihat.settings"
 
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", settings_module)
 
